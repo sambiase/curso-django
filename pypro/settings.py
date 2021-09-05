751f96cc17a2367bc136c1d8413111b4aef0ff70
sambiase <marsamb@gmail.com>
2021-09-05 14:48:10 -0300

    Added and configured lib django-s3-folder-storage for file upload in S3 AWS

--- pypro/settings.py
+++ pypro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 
@@ -126,6 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
+COLLECTFAST_ENABLED = False
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # STORAGE CONFIGURATION IN S3 AWS
@@ -136,9 +139,11 @@ if AWS_ACCESS_KEY_ID:  # checks if AWS_ACCESS_KEY_ID exists
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
-    AWS_QUERYSTRING_AUTH = True
-    AWS_S3_CUSTOM_DOMAIN = None
-    AWS_DEFAULT_ACL = 'private'
+    AWS_QUERYSTRING_AUTH = False
+    #AWS_S3_CUSTOM_DOMAIN = None
+
+    COLLECTFAST_ENABLED = True
+    AWS_DEFAULT_ACL = None
 
     # static assets
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
