10d11f0cbb715c800876108af0605b7239043a18
Martinho Sambiase <martinho.sambiase@dremio.com>
2021-12-12 13:54:10 -0300

    changed django files

--- pypro/base/views.py
+++
@@ -1,9 +0,0 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-
-
-def home(request):
-    return HttpResponse('127.0.0.1 sweet 127.0.0.1')
