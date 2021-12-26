1e74d8c27a1dc8a12a4395767f88f52150eb94eb
Martinho Sambiase <martinho.sambiase@dremio.com>
2021-12-26 16:54:41 -0300

    changed some files

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pypro.base.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # '' raiz do projeto, home = funcao home criada no passo anterior
 ]
