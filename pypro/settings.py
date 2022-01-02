9ce0850ead291ac8b7da9700122f36b68314f0d8
Martinho Sambiase <martinho.sambiase@dremio.com>
2022-01-02 13:57:36 -0300

    added more info to settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -125,6 +126,42 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+COLLECTFAST_ENABLED = False  # ambiente de Dev local
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+
+# STORAGE CONFIG FOR S3
+# verifica se a variavel AWS_ACCESS_KEY_ID nao esta vazia
+
+if AWS_ACCESS_KEY_ID:
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    # AWS_S3_OBJECT_PARAMETERS = {'CacheControl' : 'max-age=86400',} # controle de tempo de Cache do S3
+    # AWS_PRELOAD_METADATA = True
+    # AWS_AUTO_CREATE_BUCKET = False # nao iremos criar buckets automaticamente
+    # AWS_QUERYSTRING_AUTH = True #gera urls assinadas
+    # AWS_S3_CUSTOM_DOMAIN = True
+    # AWS_DEFAULT_ACL = 'private'
+
+    COLLECTFAST_ENABLED = True  # ambiente remoto de Des
+    # Static Assets
+    # -------------------------------------------------------
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATIC_S3_PATH = 'static'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+    # Upload Media Folder
+    # -------------------------------------------------------
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    DEFAULT_S3_PATH = 'media'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
+
+    INSTALLED_APPS.append('s3_folder_storage')
+    INSTALLED_APPS.append('storages')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
