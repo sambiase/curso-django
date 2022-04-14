ee26fd56b7ca7b7701b8bb200dadb3487e021e20
sambiase <martinho.sambiase@dremio.com>
2022-04-14 11:13:20 -0300

    new files

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Olá Django')
+    return HttpResponse('<html><body>Olá Django</body></html>',content_type='text/html')
