f0bf8fcba01698ea5e98745de24293106374f1ef
Martinho Sambiase <martinho.sambiase@dremio.com>
2022-04-15 16:23:11 -0300

    new files

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,6 +1,12 @@
 from django.test import Client
 
+from pypro.django_assertions import assert_contains
+
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200  # certificar que deu tudo certo
+
+def test_home_title (client: Client):
+    resp = client.get('/')
+    assert_contains (resp, '<title>Curso Django!</title>')
--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
-
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
+    return render(request, 'base/home.html')
     # raise ValueError()
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    # return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
