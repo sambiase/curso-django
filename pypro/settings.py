fcc471ae85f9541645074eeab26623aa515b4434
Sambiase <marsamb@gmail.com>
2022-09-14 16:03:23 -0300

    added videos app

--- pypro/settings.py
+++ pypro/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pypro.base',
     'pypro.videos',
+
 ]
 
 MIDDLEWARE = [
---
+++ pypro/videos/tests.py
@@ -0,0 +1,22 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(
+        reverse('videos:video', args=('Binance_Bep20',)))  # Binance Bep20 is the slug that goes into urls.py <slug>
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_video_title(resp):
+    assert_contains(resp, '<h1>Binance_Bep20</h1>')
+
+
+def test_video_content(resp):
+    assert_contains(resp, '<iframe src="https://www.youtube.com/embed/vO_BGXQFwo4"')
