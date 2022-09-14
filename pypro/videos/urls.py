6a28d83b64b12c73ab0eaf62bdbd16cec88a15ca
Sambiase <marsamb@gmail.com>
2022-09-14 16:03:23 -0300

    added videos app

---
+++ pypro/videos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from pypro.videos.views import video
+
+app_name = 'videos'
+urlpatterns = [
+    path('<slug:slug>', video, name='video'),  # '' raiz do projeto, home = funcao home criada no passo anterior
+]
